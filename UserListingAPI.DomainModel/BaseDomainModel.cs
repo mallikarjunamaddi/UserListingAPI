61722fdc708d909ddae783f329643c9b3aadb3c9
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-14 20:24:59 +0530

    Included Domain Model

---
+++ UserListingAPI.DomainModel/BaseDomainModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UserListingAPI.DomainModel
+{
+	public class BaseDomainModel
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ UserListingAPI.DomainModel/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserListingAPI.DomainModel
+{
+	public class User : BaseDomainModel
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string RoleType { get; set; }
+		public string Status { get; set; }
+	}
+}
