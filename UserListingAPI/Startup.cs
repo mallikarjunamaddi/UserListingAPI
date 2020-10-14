d5a24d66a5c58e8c28da889b758ea7d61815fca7
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-14 18:20:01 +0530

    Integrated Swagger UI.

--- UserListingAPI/Startup.cs
+++ UserListingAPI/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using Microsoft.OpenApi.Models;
 
 namespace UserListingAPI
 {
@@ -25,6 +23,11 @@ namespace UserListingAPI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			// Register Swagger 
+			services.AddSwaggerGen(c =>
+			   c.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "User Listing API", Version = "V1" })
+		   );
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +37,12 @@ namespace UserListingAPI
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			// Add Swagger UI
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+				c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "User Listing API Version 1")
+			);
 
 			app.UseRouting();
 
