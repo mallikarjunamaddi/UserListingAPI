d15a6df9f8bb29c9f7810db79a8f059b167bb726
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-15 11:05:46 +0530

    Added EndPoint for Get, Post in UserController

---
+++ UserListingAPI/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using UserListingAPI.Business.BusinessContracts;
+
+
+namespace UserListingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+		private readonly IUserBusiness _userBusiness;
+
+		public UsersController(IUserBusiness userBusiness)
+        {
+            _userBusiness = userBusiness;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public ActionResult<DomainModel.User[]> GetUsers()
+        {
+			var result = _userBusiness.GetUsers(); 
+			return Ok(result);
+        }
+
+		// POST: api/Users
+		[HttpPost]
+		public ActionResult<DomainModel.User> PostUser(DomainModel.User user)
+		{
+			var result = _userBusiness.AddUser(user);
+			return Ok(result);
+		}
+
+	}
+}
