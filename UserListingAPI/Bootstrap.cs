4b1f5a8658ad8f31aa529fd56104e824d3f7d639
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-15 10:42:32 +0530

    Registering Repository and Business with DI Container.

---
+++ UserListingAPI/Bootstrap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using UserListingAPI.Business;
+using UserListingAPI.Repository;
+using UserListingAPI.Business.BusinessContracts;
+using UserListingAPI.Repository.RepositoryContracts;
+
+namespace UserListingAPI
+{
+	public class Bootstrap
+	{
+        /// <summary>
+        /// Register the Dependencies of Business and Repository
+        /// </summary>
+        /// <param name="services"></param>
+        public static void Register(IServiceCollection services)
+        {
+            // Register dependency of Business layer.
+            services.AddScoped<IUserBusiness, UserBusiness>();
+
+            // Register dependency of Repository layer
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
--- UserListingAPI/Startup.cs
+++ UserListingAPI/Startup.cs
@@ -35,6 +35,9 @@ namespace UserListingAPI
 			services.AddSwaggerGen(c =>
 			   c.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "User Listing API", Version = "V1" })
 		   );
+
+			// Registering the application specific services.
+			Bootstrap.Register(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
